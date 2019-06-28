e959eadeafd751c038d2838ad5dfa5c8c0b17b7b
iLinked1337 <43618132+iLinked1337@users.noreply.github.com>
2019-06-28 18:18:28 +0200

    Update Program.cs

--- dntest/Program.cs
+++ dntest/Program.cs
@@ -13,7 +13,6 @@ namespace dntest
     {
         static void Main()
         {
-            DiscordClient client = new DiscordClient("NTIxNjA4OTQ0ODM0MDUyMTE3.XRD0qg._rq_oHsT20Fxt5UH2wQ7R5_iJ_0");
         }
     }
 }
